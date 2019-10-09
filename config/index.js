c25ee62b2e970be7731c8313d85607a171e5e69d
Guidozijef <913497146@qq.com>
2019-10-09 11:08:48 +0800

    取消代理

--- config/index.js
+++ config/index.js
@@ -8,13 +8,13 @@ module.exports = {
   dev: {
 
     proxyTable: {
-        '/api': {
-            target: 'http://localhost:8080/',//设置你调用的接口域名和端口号 别忘了加http
-            changeOrigin: true,
-            pathRewrite: {
-              '^/api': '' //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'https://api.douban.com/user/add'，直接写‘/api/user/add’即可，此处的‘api’可以设置为自己想要设置的任何词语，符合规范即可
-            }
-        }
+        // '/api': {
+        //     target: 'http://localhost:8080/',//设置你调用的接口域名和端口号 别忘了加http
+        //     changeOrigin: true,
+        //     pathRewrite: {
+        //       '^/api': '' //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'https://api.douban.com/user/add'，直接写‘/api/user/add’即可，此处的‘api’可以设置为自己想要设置的任何词语，符合规范即可
+        //     }
+        // }
     },
 
     // Paths
