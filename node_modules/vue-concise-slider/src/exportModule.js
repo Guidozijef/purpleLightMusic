bcdd1d6ee1ec1376bd35cbfe92383f0297b2f5cd
张剑锋 <913497146@qq.com>
2022-12-02 17:24:44 +0800

    删除node_modules

--- node_modules/vue-concise-slider/src/exportModule.js
+++
@@ -1,6 +0,0 @@
-import sliderComponent from './components/slider'
-import sliderItemComponent from './components/slider_item'
-const slider = sliderComponent
-const slideritem = sliderItemComponent
-export default slider
-export { slider, slideritem }
